c0f5155487fb85dc1838690b2b7165b6b43f6668
Joel Hutchinson <hutchy100@live.co.uk>
2025-05-15 13:06:07 -0300

    add whitespace

--- composables/profile.ts
+++ composables/profile.ts
@@ -4,10 +4,12 @@ export const useProfile = () => {
   const user = useSupabaseUser();
 
   const { data: profile } = useNuxtData<Profile>(key);
+
   const refresh = async () => {
     clearNuxtData(key);
     await refreshNuxtData(key);
   };
+
   const fetch = async () => {
     await useFetch(`/api/profiles/${user.value.id}`, { key });
   };
--- composables/puzzle.ts
+++ composables/puzzle.ts
@@ -2,10 +2,12 @@ const key = "puzzle";
 
 export const usePuzzle = (id: string) => {
   const { data: puzzle } = useNuxtData<Puzzle>(key);
+
   const refresh = async () => {
     clearNuxtData(key);
     await refreshNuxtData(key);
   };
+
   const fetch = async () => {
     await useFetch(`/api/puzzles/${id}`, { key });
   };
