889fe8c0064da1da2aa12db58ff5ca9a5254f9f8
Joel Hutchinson <hutchy100@live.co.uk>
2025-05-15 01:47:20 -0300

    implemented api endpoints for retrieving the current puzzle in a puzzle set, and updating after a solve

---
+++ server/api/profiles/[id]/puzzle-sets/[name]/current/index.get.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+const routeSchema = z.object({
+  id: z
+    .string({
+      required_error: "Profile ID is required",
+      invalid_type_error: "Profile ID must be a string",
+    })
+    .uuid({ message: "Profile ID must be a UUID" }),
+  name: z.string({
+    required_error: "Puzzle set name is required",
+    invalid_type_error: "Puzzle set name must be a string",
+  }),
+});
+
+export default defineEventHandler(async (event) => {
+  await authorize(event);
+
+  const { id, name } = await getZodValidatedRouterParams(event, routeSchema);
+
+  return await fetchCurrentPuzzleInSet(event, {
+    profileId: id,
+    puzzleSetName: name,
+  });
+});
--- server/utils/puzzle-sets.ts
+++ server/utils/puzzle-sets.ts
@@ -2,6 +2,7 @@ import type { H3Event } from "h3";
 import { serverSupabaseClient } from "#supabase/server";
 import { PuzzleSetPuzzle } from "~/types/types";
 
+// TODO: Implement
 export const generatePuzzleSet = async (
   event: H3Event,
   params: {
@@ -36,6 +37,7 @@ export const generatePuzzleSet = async (
   return { name: params.name, puzzles };
 };
 
+// TODO: Implement
 export const createPuzzleSet = async (
   event: H3Event,
   params: { puzzleSet: PuzzleSet }
@@ -72,4 +74,119 @@ export const createPuzzleSet = async (
   }
 
   return data;
+};
+
+export const fetchPuzzleSetByName = async (
+  event: H3Event,
+  params: { profileId: string; name: string }
+) => {
+  const supabase = await serverSupabaseClient<Database>(event);
+  const { data, error } = await supabase
+    .from("puzzle_sets")
+    .select("*")
+    .match({
+      profile_id: params.profileId,
+      name: params.name,
+    })
+    .single();
+
+  if (error) {
+    // content not found (error due to .single()) -> 404 error
+    if (error.code === "PGRST116")
+      throw createError({
+        statusCode: 404,
+        statusMessage: `Puzzle set with name "${params.name}" not found for profile ID "${params.profileId}".`,
+      });
+
+    // other errors -> 500 error
+    throw createError({
+      statusCode: 500,
+      message: `Error fetching puzzle set. (Message: ${error.message})`,
+    });
+  }
+
+  return data;
+};
+
+export const fetchCurrentPuzzleInSet = async (
+  event: H3Event,
+  params: { profileId: string; puzzleSetName: string }
+) => {
+  const supabase = await serverSupabaseClient<Database>(event);
+  const { data, error } = await supabase
+    .from("puzzle_set_puzzles")
+    .select("puzzles(*), puzzle_sets(*)")
+    .eq("puzzle_sets.profile_id", params.profileId)
+    .eq("puzzle_sets.name", params.puzzleSetName)
+    .eq("is_solved", false)
+    .order("index", { ascending: true })
+    .limit(1)
+    .single();
+
+  if (error) {
+    // content not found (error due to .single()) -> 404 error
+    if (error.code === "PGRST116")
+      throw createError({
+        statusCode: 404,
+        statusMessage: `Current puzzle not found for puzzle set "${params.puzzleSetName}" profile ID "${params.profileId}".`,
+      });
+
+    // other errors -> 500 error
+    throw createError({
+      statusCode: 500,
+      message: `Error fetching puzzle. (Message: ${error.message})`,
+    });
+  }
+
+  return data.puzzles;
+};
+
+export const updateCurrentPuzzleInSetSolved = async (
+  event: H3Event,
+  params: { profileId: string; puzzleSetName: string }
+) => {
+  // get the id of the puzzle_set_puzzles record
+  const supabase = await serverSupabaseClient<Database>(event);
+  const { data: fetchData, error: fetchError } = await supabase
+    .from("puzzle_set_puzzles")
+    .select("id, puzzle_sets(*)")
+    .eq("puzzle_sets.profile_id", params.profileId)
+    .eq("puzzle_sets.name", params.puzzleSetName)
+    .eq("is_solved", false)
+    .order("index", { ascending: true })
+    .limit(1)
+    .single();
+
+  console.log(fetchData);
+
+  if (fetchError) {
+    // content not found (error due to .single()) -> 404 error
+    if (fetchError.code === "PGRST116")
+      throw createError({
+        statusCode: 404,
+        statusMessage: `Current puzzle not found for puzzle set "${params.puzzleSetName}" profile ID "${params.profileId}".`,
+      });
+
+    // other errors -> 500 error
+    throw createError({
+      statusCode: 500,
+      message: `Error fetching puzzle. (Message: ${fetchError.message})`,
+    });
+  }
+
+  // set the is_solved field to true
+  const { data: updateData, error: updateError } = await supabase
+    .from("puzzle_set_puzzles")
+    .update({ is_solved: true })
+    .eq("id", fetchData.id)
+    .select();
+
+  if (updateError) {
+    throw createError({
+      statusCode: 500,
+      message: `Error updating puzzle. (Message: ${updateError.message})`,
+    });
+  }
+
+  return updateData;
 };
