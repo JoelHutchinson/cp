e058d823430dacd031ae9aa3356fd5bdb9bf46d4
Joel Hutchinson <hutchy100@live.co.uk>
2025-06-02 20:01:03 -0300

    add link to favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
+    },
+  },
   compatibilityDate: "2024-04-03",
   imports: {
     dirs: ["types"],
