ef1edf6237fe0ce7dd9742cba70e59a3f2843007
Joel Hutchinson <hutchy100@live.co.uk>
2025-06-08 20:42:00 -0300

    refactor: simplify privacy policy page structure

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="prose dark:prose-invert max-w-none">
-    <iframe
-      src="/privacy-policy.html"
-      style="width: 100%; height: 100%; border: none"
-    ></iframe>
-  </div>
+  <iframe
+    src="/privacy-policy.html"
+    style="width: 100%; height: 100%; border: none"
+  ></iframe>
 </template>
