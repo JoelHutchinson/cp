0c9292867e1591e58d4c7d274dbfd77038185e32
Joel Hutchinson <hutchy100@live.co.uk>
2025-06-16 19:35:27 -0300

    feat: improve styling of cookie policy

--- pages/cookie-policy.vue
+++ pages/cookie-policy.vue
@@ -1,6 +1,14 @@
 <template>
   <UiHeading class="mb-6">Cookie Policy</UiHeading>
-  <UCard class="w-full">
+
+  <UCard>
+    <template #header>
+      <div class="flex justify-between items-center">
+        <span class="font-bold">Last updated June 08, 2025 </span>
+        <AppPrivacyPolicyLogo />
+      </div>
+    </template>
+
     <div
       class="prose dark:prose-invert p-4 !max-w-none prose-a:text-primary-500 dark:prose-a-primary-400"
     >
