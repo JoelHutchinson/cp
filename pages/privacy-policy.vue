73314aa52b148845bd2032dbe321e23f13acf449
Joel Hutchinson <hutchy100@live.co.uk>
2025-06-16 19:24:09 -0300

    feat: Improve privacy policy appearance

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,8 +1,15 @@
 <template>
   <UiHeading class="mb-6">Privacy Policy</UiHeading>
   <UCard class="w-full">
+    <template #header>
+      <div class="flex justify-between items-center">
+        <span class="font-bold">Last updated June 08, 2025 </span>
+        <AppPrivacyPolicyLogo />
+      </div>
+    </template>
+
     <div
-      class="prose dark:prose-invert p-4 !max-w-none prose-a:text-primary-500 dark:prose-a-primary-400"
+      class="prose dark:prose-invert !max-w-none prose-a:text-primary-500 dark:prose-a-primary-400"
     >
       <div v-html="htmlContent"></div>
     </div>
