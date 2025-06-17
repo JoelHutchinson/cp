d08413297a80621cbf5e4aa7d0faaa3525b0cb0a
Joel Hutchinson <hutchy100@live.co.uk>
2025-06-17 12:28:14 -0300

    feat: update authentication middleware to remove root path redirection

--- middleware/01.auth.global.ts
+++ middleware/01.auth.global.ts
@@ -4,7 +4,4 @@ export default defineNuxtRouteMiddleware(async (to) => {
   // If the user is not authenticated and trying to access a protected route, redirect to login
   if (!user.value && to.path !== "/login" && to.path !== "/register")
     return navigateTo("/login");
-
-  // If the user is authenticated and trying to access the root path, redirect to solve
-  if (user.value && to.path === "/") return navigateTo("/solve");
 });
