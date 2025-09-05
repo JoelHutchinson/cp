3186660bac1aa99d6a29b2a5a8625d1eeb129a45
Joel Hutchinson <hutchy100@live.co.uk>
2025-09-04 23:37:25 -0300

    feat: increase refresh interval to 40s

--- pages/solve/index.vue
+++ pages/solve/index.vue
@@ -68,17 +68,17 @@
 
     <!-- Desktop / Table 720x90 Leaderboard Ad -->
     <div id="adstera-leaderboard" class="hidden sm:block sm:col-start-1 sm:col-end-3 sm:row-start-3 sm:row-end-4">
-      <AdsterraLeaderboardAd :refresh-interval="30000" />
+      <AdsterraLeaderboardAd :refresh-interval="40000" />
     </div>
 
     <!-- Desktop 160x600 Sidebar Ad -->
     <div id="adstera-sidebar" class="col-start-3 col-end-4 row-start-1 row-end-3 hidden xl:block">
-      <AdsterraSidebarAd :refresh-interval="30000" />
+      <AdsterraSidebarAd :refresh-interval="40000" />
     </div>
 
     <!-- Mobile 320x50 Ad -->
      <div id="adstera-mobile" class="sm:hidden row-start-1 row-end-2 items-center justify-center">
-      <AdsterraMobileAd :refresh-interval="30000" />
+      <AdsterraMobileAd :refresh-interval="40000" />
      </div>
   </div>
 </template>
