e0df48d5d7e5b7ee5e21ebe83012139417384a2d
Joel Hutchinson <hutchy100@live.co.uk>
2025-09-04 21:25:55 -0300

    ui: move mobile ad to the top

--- pages/solve/index.vue
+++ pages/solve/index.vue
@@ -17,13 +17,13 @@
   <div
     v-else
     class="size-full grid
-    grid-rows-[auto_1fr_1fr_50px] grid-cols-1 items-center
+    grid-rows-[50px_auto_1fr_1fr] grid-cols-1 items-center
     sm:gap-4 sm:p-4 sm:grid-rows-2 sm:grid-cols-[minmax(400px,600px)_minmax(200px,350px)] sm:items-stretch
     md:p-0 md:grid-rows-2 md:grid-cols-[600px_minmax(200px,350px)]
     xl:grid-rows-2 xl:grid-cols-[600px_minmax(200px,350px)_minmax(200px,350px)]">
     <!-- Puzzle Details -->
     <div
-      class="row-start-1 row-end-2 sm:row-start-2 sm:row-end-3 sm:col-start-2 sm:col-end-3 h-full"
+      class="row-start-2 row-end-3 sm:row-start-2 sm:row-end-3 sm:col-start-2 sm:col-end-3 h-full"
     >
       <ChessPuzzleDetails
         v-if="puzzleBoardState"
@@ -39,7 +39,7 @@
 
     <!-- Puzzle Set Details -->
     <div
-      class="row-start-3 row-end-4 sm:row-start-1 sm:row-end-2 sm:col-start-2 sm:col-end-3 h-full"
+      class="row-start-4 row-end-5 sm:row-start-1 sm:row-end-2 sm:col-start-2 sm:col-end-3 h-full"
     >
       <ChessPuzzleSetDetails
         v-if="selectedPuzzleSetSlug && puzzleSets && puzzleSetProgress"
@@ -58,7 +58,7 @@
       @solved="solvePuzzle"
       @correct-move="makePuzzleMove(true)"
       @incorrect-move="makePuzzleMove(false)"
-      class="row-start-2 row-end-3 max-w-[400px] place-self-center sm:max-w-full sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-3"
+      class="row-start-3 row-end-4 max-w-[400px] place-self-center sm:max-w-full sm:col-start-1 sm:col-end-2 sm:row-start-1 sm:row-end-3"
     />
     <USkeleton
       v-else
@@ -71,7 +71,7 @@
     </div>
 
     <!-- Mobile 320x50 Ad -->
-     <div id="adstera-mobile" class="sm:hidden row-start-4 row-end-5 items-center justify-center">
+     <div id="adstera-mobile" class="sm:hidden row-start-1 row-end-2 items-center justify-center">
       <AdsterraMobileAd  />
      </div>
   </div>
